4463ef378d49b61bee6e4099aa7ff8834ed3a461
Nguyen Thien Ngon <nguyenthienngon1230@gmail.com>
2023-05-04 14:23:11 +0700

    first update

---
+++ main.js
@@ -0,0 +1,26 @@
+// --------------------------------------gallery js------------------------------------------
+function openImg1()
+{
+    window.location.href="vietnam.html";
+}
+function openImg2()
+{
+    window.location.href="dubai.html";
+}
+function openImg3()
+{
+    window.location.href="thailan.html";
+}
+function openImg4()
+{
+    window.location.href="hanquoc.html";
+}
+function openImg5()
+{
+    window.location.href="anh.html";
+}
+function openImg6()
+{
+    window.location.href="mad.html";
+}
+// --------------------------------------end gallery js------------------------------------------
